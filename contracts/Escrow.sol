243f25403267758289d376bdaee3d76a31acda23
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2023-06-16 16:45:19 +0100

    Update Escrow.sol

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,57 +1,84 @@
 // SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
 
 import "@aave/protocol-v2/contracts/interfaces/ILendingPoolAddressesProvider.sol";
 import "@aave/protocol-v2/contracts/interfaces/IERC20.sol";
 
+
+
 contract Escrow {
-    address public client;  // Client's address
-    address public aaveStablecoinPool;  // Aave Stablecoin Pool's address
-    uint256 public payment;  // Payment amount from the client
-    uint256 public releaseTime;  // Minimum time before funds are claimable
-    
-    bool public clientConfirmed;  // Flag indicating if the client has confirmed the claim
-    
-    event FundsDeposited(address indexed client, uint256 amount);
-    event ClaimInitiated(address indexed client, uint256 amount);
-    event FundsClaimed(address indexed client, uint256 amount);
-    
-    constructor(address _aaveStablecoinPool, uint256 _releaseTime) {
-        aaveStablecoinPool = _aaveStablecoinPool;
-        releaseTime = block.timestamp + _releaseTime;
+    struct Payment {
+        uint256 amount;
+        address client;
+        address freelancer;
+        bool isPaid;
+        bool isCompleted;
+        uint256 completionDate;
     }
     
-    // Function to deposit funds into the escrow
-    function depositFunds() external payable {
-        require(msg.value > 0, "Amount must be greater than zero");
-        require(client == address(0), "Funds already deposited");
-        
-        client = msg.sender;
-        payment = msg.value;
-        
-        emit FundsDeposited(client, payment);
+    Payment[] public payments;
+    ILendingPool public lendingPool;
+    
+    event PaymentCreated(uint256 paymentId);
+    event PaymentCompleted(uint256 paymentId);
+    event PaymentClaimed(uint256 paymentId);
+    
+    constructor(address _lendingPoolAddress) {
+        lendingPool = ILendingPool(_lendingPoolAddress);
     }
     
-    // Function to initiate the claim after the minimum time has passed
-    function initiateClaim() external {
-        require(msg.sender == client, "Only the client can initiate the claim");
-        require(block.timestamp >= releaseTime, "Funds are not yet claimable");
-        require(!clientConfirmed, "Funds already claimed");
-        
-        clientConfirmed = true;
-        
-        emit ClaimInitiated(client, payment);
+    modifier onlyClient(uint256 paymentId) {
+        require(msg.sender == payments[paymentId].client, "Only the client can call this function");
+        _;
     }
     
-    // Function to claim the funds from the escrow after the claim has been initiated
-    function claimFunds() external {
-        require(msg.sender == client, "Only the client can claim the funds");
-        require(clientConfirmed, "Claim not yet initiated");
+    modifier onlyFreelancer(uint256 paymentId) {
+        require(msg.sender == payments[paymentId].freelancer, "Only the freelancer can call this function");
+        _;
+    }
+    
+    function createPayment(address _freelancer) external payable {
+        Payment memory newPayment = Payment({
+            amount: msg.value,
+            client: msg.sender,
+            freelancer: _freelancer,
+            isPaid: true,
+            isCompleted: false,
+            completionDate: 0
+        });
         
-        IERC20 stablecoin = IERC20(ILendingPoolAddressesProvider(aaveStablecoinPool).getLendingPool().getReserveData(address(this)).underlyingAsset);
+        payments.push(newPayment);
+        emit PaymentCreated(payments.length - 1);
+    }
+    
+    function confirmCompletion(uint256 paymentId) external onlyClient(paymentId) {
+        require(payments[paymentId].isPaid, "Payment has not been made");
+        require(!payments[paymentId].isCompleted, "Payment has already been completed");
         
-        require(stablecoin.balanceOf(address(this)) >= payment, "Insufficient funds in the escrow");
-        require(stablecoin.transfer(client, payment), "Failed to transfer funds");
+        payments[paymentId].isCompleted = true;
+        payments[paymentId].completionDate = block.timestamp;
         
-        emit FundsClaimed(client, payment);
+        emit PaymentCompleted(paymentId);
+    }
+    
+    function claimPayment(uint256 paymentId) external onlyFreelancer(paymentId) {
+        require(payments[paymentId].isPaid, "Payment has not been made");
+        require(payments[paymentId].isCompleted, "Payment has not been completed");
+        require(block.timestamp >= payments[paymentId].completionDate + 15 days, "Payment cannot be claimed yet");
+        
+        uint256 amountToClaim = payments[paymentId].amount;
+        lendingPool.withdraw(address(this), amountToClaim);
+        
+        payable(payments[paymentId].freelancer).transfer(amountToClaim);
+        
+        emit PaymentClaimed(paymentId);
+    }
+    
+    function getPaymentCount() external view returns (uint256) {
+        return payments.length;
+    }
+    
+    function getPayment(uint256 paymentId) external view returns (Payment memory) {
+        return payments[paymentId];
     }
 }
